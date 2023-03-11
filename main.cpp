10e0464561bc27d75d7c7b6dfed18f795bf13a7a
ihita2812 <101123081+ihita2812@users.noreply.github.com>
2023-03-11 19:49:12 +0530

    Create main.cpp
    (This will be the file that gets executed finally)

---
+++ main.cpp
@@ -0,0 +1,38 @@
+/* 
+
+PURPOSE OF FILE:
+handles input and output, invokes simulator
+
+*/
+
+#include<RISCV.h>
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+
+int main(int argc, char** argv) {
+  char* prog_mem_file;
+  int n;
+  if(argc < 2) {
+    printf("Incorrect number of arguments. Invoke the simulator: \n\t./myRISCVSim <input mem file> [<vale of N>]\n");
+    exit(1);
+  } else if(argc == 2) {
+    printf("Since value of N not provided, taking default value N = 5\n\n");
+    n = 5;
+  } else if(argc == 3) {
+    n = stoi(argv[2]);
+  } else {
+    printf("Incorrect call of program. Please check for typos. Invoke the simulator: \n\t./myRISCVSim <input mem file> [<value of N>]\n");
+    exit(1);
+  }
+  
+  //reset the processor
+  reset_proc();
+  //load the program memory
+  load_memory(argv[1], n);
+  //run the simulator
+  run();
+
+  return 0;
+}
