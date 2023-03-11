5dd841d7b77d6f94646200fc3ed5300d132bdcec
ihita2812 <101123081+ihita2812@users.noreply.github.com>
2023-03-11 20:06:08 +0530

    defined functions called directly in main()
    defined:
    1. reset_proc
    2. load_memory
    3. run

--- myRISCV.cpp
+++ myRISCV.cpp
@@ -1,18 +1,60 @@
 /* 
-This Program is developed as part of the Computer Architecture class
-Project Name: RISCV 32I Emulator
 
-Developer's Name: Rhitvik Anand
-Developer's EMail: rhitvik2000anand@gmail.com || 2021csb1127@iitrpr.ac.in
+PURPOSE OF FILE:
+implementation file for simulator
 
-*/
+[this is where you have to define all the fetch decode etc wale functions]
 
-/* 
-main.cpp 
-Purpose of this file:
 */
 
 #include<RISCV.h>
 #include<bits/stdc++.h>
 #include<stdio.h>
 #include<stdlib.h>
+
+//reset_proc() is called to reset the processor to default values
+void reset_proc() {
+  
+  //setting empty memory:
+  //please explain to me how the memory map thingy works
+  for (int i = 0; i < MEM_SIZE; i++) {
+    MEM[i] = 0x0;
+  }
+  
+  //setting empty registers:
+  //do we wanna put default values in registers? like global pointer stack pointer etc? (i think we should)
+  for (int i= 0; i < ADD_LEN; i++) {
+    reg[i] = 0x0;
+  }
+}
+
+//load_program_memory(char *file_name, int n) pupulates the instruction memory from file_name and stores value of N (required for all test files) to x3 in register file
+void load_memory(char *file_name, int N) {
+  
+  //instruction memory:
+  FILE *fp;
+  unsigned int address, instruction;
+  fp = fopen(file_name, "r");
+  if(fp == NULL) {
+    printf("Error opening input file!\n");
+    exit(1);
+  }
+  while(fscanf(fp, "%x %x", &address, &instruction) != EOF) {
+    write_word(MEM, address, instruction);
+  }
+  fclose(fp);
+  
+  //storing value of N:
+  reg[3] = N;
+}
+
+//run() starts the actual single cycle processor simulator
+void run() {
+  while(1) {
+    fetch();
+    decode();
+    execute();
+    mem();
+    write_back();
+  }
+}
