fea34fd4ef41ca74b6ff262cfc67249782cd2d3f
ihita2812 <101123081+ihita2812@users.noreply.github.com>
2023-03-12 17:44:28 +0530

    sorry

--- RISCV.h
+++ RISCV.h
@@ -5,43 +5,6 @@
 #define ADD_LEN 32
 #define MEM_SIZE 0x10000
 
-int clock_cycle = 0;
-int instruction[32];
-FILE* programcode;
-
-//ALU Variables
-int op_code = 0;
-int rd = 0;
-int f3 = 0;
-int rs1 = 0;
-int rs2 = 0;
-int f7 = 0;
-int immI = 0;
-int immS = 0;
-int immB = 0;
-int immU = 0;
-int immJ = 0;
-
-//Control Lines
-int ALUres = 0;
-int LoadType = 0;
-int StoreType = 0;
-int TakeBranch = 0;
-int MemRead = 0;
-int MemWrite = 0;
-int RegWrite = 0;
-
-int op1;
-int op2;
-
-uint32_t MemAdr = 0;
-uint32_t LoadData = 0;
-std::map<uint32_t, uint8_t> memory;
-
-//map<MemAdr, ByteMemory>
-/*Since byte is the smallest unit of memory being loaded or stored, 
-the map used in this program will have 1-byte addressing.*/
-
 class RISCV{
 
 public:
