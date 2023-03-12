43458a7d6d3ff31488239560a9ff9007e2d700e3
Niti <115517964+n1ti@users.noreply.github.com>
2023-03-12 17:20:35 +0530

    class error solved

--- RISCV.h
+++ RISCV.h
@@ -5,7 +5,7 @@
 #define ADD_LEN 32
 #define MEM_SIZE 0x10000
 
-int clock = 0;
+int clock_cycle = 0;
 int instruction[32];
 FILE* programcode;
 
@@ -42,7 +42,34 @@ std::map<uint32_t, uint8_t> memory;
 /*Since byte is the smallest unit of memory being loaded or stored, 
 the map used in this program will have 1-byte addressing.*/
 
-class RISCV{};
+class RISCV{
+
+public:
+    //constructor function
+    RISCV()
+    {
+        pc = 0x0;
+        //setting empty memory:
+        memory.clear();
+        //setting empty registers:
+        for (int i= 0; i < ADD_LEN; i++) {
+             reg[i] = 0x0;
+        }
+        reg[2] = 0x7FFFFFF0; //Stack Pointer
+        reg[3] = 0x10000000; //Data Pointer
+    }
+    
+    uint32_t reg[32];
+    uint32_t pc;
+    RISCV(char* file_name, uint32_t mem_start);
+    void run();
+    void fetch();
+    void decode();
+    void execute();
+    void mem();
+    void write_back();
+};
+
 
 void run();
 void load_memory(char* file_name, int N);
