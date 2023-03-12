e616027a40884039e8b81598ba41391b703e2e3b
Niti <115517964+n1ti@users.noreply.github.com>
2023-03-12 16:16:20 +0530

    final updated

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@ handles input and output, invokes simulator
 
 */
 
-#include<RISCV.h>
-#include<bits/stdc++.h>
+#include"RISCV.h"
 #include<stdio.h>
 #include<stdlib.h>
 #include<string>
@@ -21,7 +20,7 @@ int main(int argc, char** argv) {
     printf("Since value of N not provided, taking default value N = 5\n\n");
     n = 5;
   } else if(argc == 3) {
-    n = stoi(argv[2]);
+    n = atoi(argv[2]);
   } else {
     printf("Incorrect call of program. Please check for typos. Invoke the simulator: \n\t./myRISCVSim <input mem file> [<value of N>]\n");
     exit(1);
