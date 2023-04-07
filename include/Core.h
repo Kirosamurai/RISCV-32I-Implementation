ed9fffa0aaa65ccb5b8395e39cbca08d7ac8c513
ihita2812 <101123081+ihita2812@users.noreply.github.com>
2023-04-07 11:43:38 +0530

    Uploaded all header files

---
+++ include/Core.h
@@ -0,0 +1,92 @@
+#ifndef CORE_H_
+#define CORE_H_
+
+#include <cstring>
+#include <map>
+#include <iostream>
+#include <stdlib.h>
+
+#include "Pipeline.h"
+#include "Instruction_Packet.h"
+#include "RISCV.h"
+
+class Core {
+
+public:
+
+    char *test_file;
+    int N;
+    
+    //KNOBS read from command line:
+    bool pipelining;
+    bool forwarding;
+    bool tracing;
+    int trace_instruction;
+    bool register_file;
+    bool pipeline_register;
+
+    //Store values of stats:
+    int instructions; //stat 2
+    float CPI; //stat 3
+    int data_instructions; //stat 4
+    int ALU_instructions; //stat 5
+    int control_instructions; //stat 6
+    int bubbles; //stat 7
+    int data_hazards; //stat 8
+    int control_hazards; //stat 9
+    int data_bubbles; //stat 11
+    int control_bubbles; //stat 12
+    int wrong_predicts; //stat 10
+
+    //Constructor
+    Core() {
+        
+        N = 5;
+        
+        pipelining = false;
+        forwarding = false;
+        tracing = false;
+        trace_instruction = 0;
+        register_file = false;
+        pipeline_register = false;
+
+        instructions = 0;
+        CPI = 1.00;
+        data_instructions = 0;
+        ALU_instructions = 0;
+        control_instructions = 0;
+
+        bubbles = 0;
+        data_hazards = 0;
+        control_hazards = 0;
+        data_bubbles = 0;
+        control_bubbles = 0;
+        wrong_predicts = 0;
+        
+    }
+
+    //Opening test file and defining memory
+    void loadMemory();
+
+    //Starting the actual simulator
+    void Run();
+
+    //Pipeline Functionality
+    void runCycle();
+    void Stop();
+    void showStats();
+
+};
+
+void showRegisters();
+
+extern Core CPU;
+
+extern PipelineRegister IF_DE;
+extern PipelineRegister DE_EX;
+extern PipelineRegister EX_MA;
+extern PipelineRegister MA_WB;
+
+extern RISCV processor;
+
+#endif
---
+++ include/Instruction_Packet.h
@@ -0,0 +1,54 @@
+#ifndef PACKET_H_
+#define PACKET_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+#include "RISCV.h"
+
+//Acts as a mediator before writing to register, just to store the data
+struct InstructionPacket{
+    
+    uint32_t current_pc_ip;
+    int instruction_ip[32];
+
+    int op_code_ip;
+    int rd_ip;
+    int f3_ip;
+    int rs1_ip;
+    int rs2_ip;
+    int f7_ip;
+    int immI_ip;
+    int immS_ip;
+    int immB_ip;
+    long immU_ip;
+    int immJ_ip;
+
+    int op1_ip;
+    int op2_ip;
+
+    int LoadType_ip;
+    int StoreType_ip;
+    int TakeBranch_ip;
+    int MemRead_ip;
+    int MemWrite_ip;
+    int RegWrite_ip;
+
+    long ALUres_ip;
+
+    long LoadData_ip;
+    uint32_t MemAdr_ip;
+
+    int DepFlag_ip;
+    
+};
+
+extern struct InstructionPacket IF_DE_IP;
+extern struct InstructionPacket DE_EX_IP;
+extern struct InstructionPacket EX_MA_IP;
+extern struct InstructionPacket MA_WB_IP;
+
+void WriteToPacket(struct InstructionPacket &);
+
+#endif
