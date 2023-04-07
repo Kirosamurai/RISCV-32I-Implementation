be51132457537ff298e773c045d03f733b7bcb4c
ihita2812 <101123081+ihita2812@users.noreply.github.com>
2023-04-07 11:47:33 +0530

    Update RISCV.h

--- include/RISCV.h
+++ include/RISCV.h
@@ -5,58 +5,62 @@
 #include <map>
 #include <iostream>
 #include <stdlib.h>
+#include <bitset>
 
 #define ADD_LEN 32
 #define MEM_SIZE 0x10000
 
-
-//map<MemAdr, ByteMemory>
-/*Since byte is the smallest unit of memory being loaded or stored, 
-the map used in this program will have 1-byte addressing.*/
-
 class RISCV{
 
 public:
-    bool pipeline;
-    int clock_cycle = 0;
+    int reg[32];
+    uint32_t pc;
+    uint32_t new_pc;
+    uint32_t current_pc;
+
+    int clock_cycle;
     int instruction[32];
     FILE* programcode;
 
     //ALU Variables
-    int op_code = 0;
-    int rd = 0;
-    int f3 = 0;
-    int rs1 = 0;
-    int rs2 = 0;
-    int f7 = 0;
-    int immI = 0;
-    int immS = 0;
-    int immB = 0;
-    long immU = 0;
-    int immJ = 0;
+    int op_code;
+    int rd;
+    int f3;
+    int rs1;
+    int rs2;
+    int f7;
+    int immI;
+    int immS;
+    int immB;
+    long immU;
+    int immJ;
 
     //Control Lines
-    int LoadType = 0;
-    int StoreType = 0;
-    int TakeBranch = 0;
-    int MemRead = 0;
-    int MemWrite = 0;
-    int RegWrite = 0;
+    int LoadType;
+    int StoreType;
+    int TakeBranch;
+    int MemRead;
+    int MemWrite;
+    int RegWrite;
 
-    int op1;
-    int op2;
+    long long op1;
+    long long op2;
 
-    uint32_t MemAdr = 0;
-    long LoadData = 0;
-    long ALUres = 0;
+    uint32_t MemAdr;
+    long long LoadData;
+    long long ALUres;
+
+    //map<MemAdr, ByteMemory>
+    //Since byte is the smallest unit of memory being loaded or stored, 
+    //the map used in this program will have 1-byte addressing.
     std::map<uint32_t, uint8_t> memory;
-
 
 
     //constructor function
     RISCV()
     {
         pc = 0x0;
+        new_pc = 0x0;
         //setting empty memory:
         memory.clear();
         //setting empty registers:
@@ -65,24 +69,60 @@ public:
         }
         reg[2] = 0x7FFFFFF0; //Stack Pointer
         reg[3] = 0x10000000; //Data Pointer
+
+        clock_cycle = 0;
+        for (int i= 0; i < ADD_LEN; i++) {
+            instruction[i] = 0x0;
+        }
+        
+        //ALU Variables
+        op_code = 0;
+        rd = 0;
+        f3 = 0;
+        rs1 = 0;
+        rs2 = 0;
+        f7 = 0;
+        immI = 0;
+        immS = 0;
+        immB = 0;
+        immU = 0;
+        immJ = 0;
+
+        //Control Lines
+        LoadType = 0;
+        StoreType = 0;
+        TakeBranch = 0;
+        MemRead = 0;
+        MemWrite = 0;
+        RegWrite = 0;
+
+        MemAdr = 0;
+        LoadData = 0;
+        ALUres = 0;
+
+        DepFlag = 0;
     }
-    uint32_t reg[32];
-    uint32_t pc;
-    RISCV(char* file_name, uint32_t mem_start);
-    void run();
+    
+    //For pipelining:
+    int DataDependency;
+    int ControlDependency;
+    int DepFlag;
+    bool pipeline;
+    bool forward;
+
     void fetch();
     void decode();
     void execute();
     void mem();
     void write_back();
+
+    void reset();
+    void store_memory(); 
+    void instruction_exit();
 };
 
-
-void run();
-void load_memory(char* file_name, int N);
-void reset();
 uint32_t stringtohex(char input[11]);
-void store_memory(); 
-void instruction_exit();
+
+extern RISCV processor;
 
 #endif
