d88a78887bc6896837b88110f726f6c381ccbf05
Ihita <101123081+ihita2812@users.noreply.github.com>
2023-04-24 21:21:55 +0530

    Add files via upload

---
+++ src/Cache.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <bitset>
+#include "Cache.h"
+
+bool Cache::isPresent(uint32_t add) {
+    
+    std::string address = std::bitset<32>(add).to_string();
+
+    tag = address.substr(0, tag_bits);
+    index = address.substr(tag_bits, index_bits);
+    offset = address.substr(tag_bits+index_bits, offset_bits);
+
+    int index_num;
+    for (int i=index_bits-1; i>=0; i--) {
+        index_num += index[i] * pow(2,index_bits-i-1);
+    }
+
+    for (int i=0; i<associativity; i++) {
+        if (tag_array[index_num][i] == tag) {
+            thisWay = i;
+            return true;
+        }
+    }
+
+    return false;
+
+}
+
+uint8_t Cache::read() {
+    uint8_t data_val;
+
+    int index_num;
+    for (int i=index_bits-1; i>=0; i--) {
+        index_num += index[i] * pow(2,index_bits-i-1);
+    }
+    int offset_num;
+    for (int i=offset_bits-1; i>=0; i--) {
+        offset_num += offset[i] * pow(2,offset_bits-i-1);
+    }
+
+    std::string data = data_array[index_num][3][thisWay];
+
+    for (int i=8*offset_num; i<8*(offset_num+1); i++) {
+        data_val += data[i] * pow(2,8*offset_bits+7-i);
+    }
+
+    return data_val;
+
+}
+
+void Cache::write() {
+    int index_num;
+    for (int i=index_bits-1; i>=0; i--) {
+        index_num += index[i] * pow(2,index_bits-i-1);
+    }
+
+    data_array[index_num][0][thisWay] = 1; //validity
+    data_array[index_num][1][thisWay] = 1; //dirty
+    recencyUpdater(index_num, thisWay); //recency
+}
